a347d009d86f6b18b29c6f4b7f820d8c34a62ed6
anthony perritano <aperritano@gmail.com>
2016-12-18 13:32:15 -0500

    Initial Commit

---
+++ Researcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Researcher
+//
+//  Created by Anthony Perritano on 12/18/16.
+//  Copyright © 2016 Anthony Perritano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
